a5e6095c1a0ec0a7619f142643f7a249b64e4851
admin <innovainformationtechnologies@gmail.com>
2026-06-03 02:58:41 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let config = await fetch("./static/config.json").then(response => response.json());
+let getCSVContents, buildPage
+
+if (config.debug) {
+  await import("./scripts/util.js").then(module => {
+    getCSVContents = module.getCSVContents;
+    buildPage = module.buildPage
+  });
+} else {
+  await import ("https://cdn.jsdelivr.net/gh/innovainformationtechnologies/sheetsite-util@main/util.js").then(module => {
+    getCSVContents = module.getCSVContents;
+    buildPage = module.buildPage
+  });
+  
+}
+
+let data = [];
+
+
+console.log(getCSVContents)
+
+await getCSVContents("./static/sheet.csv", null).then(data => {
+  console.log(data)
+  buildPage(data);
+});
